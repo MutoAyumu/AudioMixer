ed266756f8280ed1dc3bbdb45ccd148a3716a475
MutoAyumu <mutoayum0610@gmail.com>
2021-10-05 21:11:49 +0900

    基本機能は完成

--- Assets/AudioMixer/Script/AudioController.cs
+++ Assets/AudioMixer/Script/AudioController.cs
@@ -12,18 +12,34 @@ public class AudioController : MonoBehaviour
     //どのボタンを押したときに設定画面を出すか
     [SerializeField] string m_getButtonName = " ";
     //ゲーム開始時の音量
-    float m_startVolume = -15f;
+    float m_startVolumeMaster = -15f;
+    float m_startVolumeBGM = -15f;
+    float m_startVolumeSE = -15f;
+    //ゲームシーンにこのオブジェクトが存在するかを判定するもの
+    public static AudioController m_instance;
 
+    private void Awake()
+    {
+        if (m_instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            m_instance = this;
+        }
+    }
     private void Start()
     {
-        SetMaster(m_startVolume);
-        SetBGM(m_startVolume);
-        SetSE(m_startVolume);
+        SetMaster(m_startVolumeMaster);
+        SetBGM(m_startVolumeBGM);
+        SetSE(m_startVolumeSE);
     }
 
     private void Update()
     {
-        if(Input.GetButtonDown(m_getButtonName))
+        if (Input.GetButtonDown(m_getButtonName))
         {
             m_canvas.gameObject.SetActive(!m_canvas.activeSelf);
         }
@@ -31,13 +47,16 @@ public class AudioController : MonoBehaviour
     public void SetMaster(float volume)
     {
         m_audioMixer.SetFloat("Master", volume);
+        m_startVolumeMaster = volume;
     }
     public void SetBGM(float volume)
     {
         m_audioMixer.SetFloat("BGM", volume);
+        m_startVolumeBGM = volume;
     }
     public void SetSE(float volume)
     {
         m_audioMixer.SetFloat("SE", volume);
+        m_startVolumeSE = volume;
     }
 }
